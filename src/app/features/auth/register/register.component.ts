3251e58879fc9b6526258ec2ecaa7832ac02d625
thomasIemaire <thomas.lmre14@gmail.com>
2025-08-04 22:51:02 +0200

    register form

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { Button } from 'primeng/button';
+
+import { IUser } from '../../../core/models/user.model';
 
 @Component({
     selector: 'app-auth-register',
-    imports: [],
+    imports: [FormsModule, RouterLink, Button, PasswordModule, InputTextModule],
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+    private USER_DEFAULT: IUser = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    };
+
+    public user: IUser = { ...this.USER_DEFAULT };
+    public step: number = 0;
+
+    private router: Router = inject(Router);
+    private route = inject(ActivatedRoute);
+
+    ngOnInit(): void {
+        this.route.queryParams.subscribe(params => {
+            console.log('RegisterComponent initialized with queryParams:', params);
+            this.step = +params['step'] || 0;
+        });
+    }
+
+    public changeStep(step: number): void {
+        this.step = step;
+        this.router.navigate(['auth/register'], { queryParams: { step } });
+    }
+
 }
