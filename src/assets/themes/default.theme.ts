54466223e04c71464d37fdd3d12d21d3c0c8aa61
Thomas Lemaire <thomas.lemaire@sendoc.fr>
2025-08-05 16:14:34 +0200

    update font

--- src/assets/themes/default.theme.ts
+++ src/assets/themes/default.theme.ts
@@ -2,21 +2,4 @@ import { input } from "@angular/core";
 import { definePreset } from "@primeuix/themes";
 import Aura from '@primeuix/themes/aura';
 
-export const defaultTheme = definePreset(Aura, {
-    // semantic: {
-    //     colorScheme: {
-    //         light: {
-    //             surface: {
-    //                 50: '{zinc.50}',
-    //                 950: '{zinc.950}'
-    //             }
-    //         },
-    //         dark: {
-    //             surface: {
-    //                 50: '{zinc.950}',
-    //                 950: '{zinc.50}'
-    //             }
-    //         }
-    //     }
-    // }
-});
+export const defaultTheme = definePreset(Aura, {});
