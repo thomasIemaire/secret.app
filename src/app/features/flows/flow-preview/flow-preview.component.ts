ea5718d80d2b45dcd52975f49f87ca7f78a8f901
Thomas Lemaire <thomas.lemaire@sendoc.fr>
2025-08-06 16:10:44 +0200

    org Flow

--- src/app/features/flows/flow-preview/flow-preview.component.ts
+++ src/app/features/flows/flow-preview/flow-preview.component.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { flows } from '../../../../_db/flows.db';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Button } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { TreeNode } from 'primeng/api';
 
 @Component({
     selector: 'app-flow-preview',
-    imports: [CommonModule, FormsModule, Button],
+    imports: [CommonModule, FormsModule, Button, SelectButtonModule, OrganizationChartModule],
     templateUrl: './flow-preview.component.html',
     styleUrls: ['./flow-preview.component.scss']
 })
@@ -17,6 +20,14 @@ export class FlowPreviewComponent {
     private route: ActivatedRoute = inject(ActivatedRoute);
 
     public flow: any = null;
+
+    public selectPreview: any = {
+        options: [
+            { label: 'Arborescence', value: 'tree' },
+            { label: 'Retour', value: 'return' },
+        ],
+        value: 'tree'
+    }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
@@ -30,5 +41,66 @@ export class FlowPreviewComponent {
 
     public back() {
         this.router.navigate(['/flows']);
+    }
+
+    data: TreeNode[] = [
+        {
+            label: 'F.C Barcelona',
+            expanded: true,
+            children: [
+                {
+                    label: 'Argentina',
+                    expanded: true,
+                    children: [
+                        {
+                            label: 'Argentina'
+                        },
+                        {
+                            label: 'France'
+                        }
+                    ]
+                },
+                {
+                    label: 'France',
+                    expanded: true,
+                    children: [
+                        {
+                            label: 'France'
+                        },
+                        {
+                            label: 'Morocco'
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+    isPanning = false;
+    startX = 0;
+    startY = 0;
+    translateX = 0;
+    translateY = 0;
+    scale = .8;
+
+    startPan(event: MouseEvent) {
+        this.isPanning = true;
+        this.startX = event.clientX - this.translateX;
+        this.startY = event.clientY - this.translateY;
+        event.preventDefault();
+    }
+
+    pan(event: MouseEvent) {
+        if (!this.isPanning) return;
+        this.translateX = event.clientX - this.startX;
+        this.translateY = event.clientY - this.startY;
+    }
+
+    endPan() {
+        this.isPanning = false;
+    }
+
+    onWheel(event: WheelEvent) {
+        const delta = event.deltaY > 0 ? -0.1 : 0.1;
+        this.scale = Math.min(Math.max(this.scale + delta, 0.5), 2);
     }
 }
