dc95f4c802a2acc9e71eae7c1abbe42ce8f65822
thomasIemaire <thomas.lmre14@gmail.com>
2025-08-08 20:50:40 +0200

    train like music

--- src/app/shared/components/organisms/sidebar/sidebar.component.ts
+++ src/app/shared/components/organisms/sidebar/sidebar.component.ts
@@ -6,10 +6,11 @@ import { IMenuItem } from "../../../../core/models/menu-item.model";
 import { UserService } from "../../../../core/services/user.service";
 import { ProgressBarModule } from 'primeng/progressbar';
 import { Tag } from "primeng/tag";
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
     selector: 'app-sidebar',
-    imports: [CommonModule, FormsModule, MenuComponent, ProgressBarModule, Tag],
+    imports: [CommonModule, FormsModule, MenuComponent, ProgressBarModule, Tag, TooltipModule],
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss']
 })
@@ -47,4 +48,31 @@ export class SidebarComponent {
             route: '/profile'
         },
     ];
+
+    public agents: any = [
+        {
+            name: 'Agent 1',
+            progress: 75,
+            version: '1.0'
+        },
+        {
+            name: 'Agent 2',
+            progress: 50,
+            version: '1.2'
+        },
+        {
+            name: 'Agent 3',
+            progress: 25,
+            version: '2.3'
+        }
+    ];
+
+    public agent: number = 0;
+
+    public setAgent(index: number): void {
+        if (index < 0)
+            this.agent = this.agents.length - 1;
+        else
+            this.agent = index % this.agents.length;
+    }
 }
