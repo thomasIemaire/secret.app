8c90040ce2271b7752a03fba8dc53e29b697644b
Thomas Lemaire <thomas.lemaire@sendoc.fr>
2025-08-28 12:26:09 +0200

    ça commence à me plaire cette histoire

--- src/_db/agents.db.ts
+++ src/_db/agents.db.ts
@@ -10,5 +10,29 @@ export let agents: any[] = [
             fullname: "Thomas Lemaire"
         },
         updated_at: '2025-08-27T09:56:03.053+00:00'
+    },
+    {
+        _id: '68ad695cf3f3331198013cf6',
+        name: 'Model de test 2',
+        description: "test d'insertion 2",
+        reference: "test2",
+        version: "1.0",
+        created_by: {
+            id: '68a7164a17d57e632ab78d51',
+            fullname: "Thomas Lemaire"
+        },
+        updated_at: '2025-08-27T09:56:03.053+00:00'
+    },
+    {
+        _id: '68ad695cf3f3331198013cf7',
+        name: 'Model de test 3',
+        description: "test d'insertion 3",
+        reference: "test3",
+        version: "1.0",
+        created_by: {
+            id: '68a7164a17d57e632ab78d51',
+            fullname: "Thomas Lemaire"
+        },
+        updated_at: '2025-08-27T09:56:03.053+00:00'
     }
 ]
--- src/app/features/agents/agents.component.ts
+++ src/app/features/agents/agents.component.ts
@@ -8,10 +8,11 @@ import { agents } from '../../../_db/agents.db';
 import { TagModule } from 'primeng/tag';
 import { Router, RouterOutlet } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
+import { DragDropModule } from 'primeng/dragdrop';
 
 @Component({
     selector: 'app-agents',
-    imports: [CommonModule, FormsModule, RouterOutlet, InputTextModule, SelectModule, ButtonModule, TagModule, AvatarModule],
+    imports: [CommonModule, FormsModule, RouterOutlet, InputTextModule, SelectModule, ButtonModule, TagModule, AvatarModule, DragDropModule],
     templateUrl: './agents.component.html',
     styleUrls: ['./agents.component.scss']
 })
@@ -19,7 +20,11 @@ export class AgentsComponent {
 
     public router: Router = inject(Router);
 
-    public agents = agents;
+    public models = agents;
+    public modelsGenerated: any[] = [];
+    public modelsToTrain: any[] = [];
+
+    public elementDragged: any = null;
 
     public search: string = '';
 
@@ -27,13 +32,65 @@ export class AgentsComponent {
 
     public filters: Array<{ [key: string]: string }> = [
         { name: 'Nom', value: 'name' },
-        { name: 'État', value: 'status' },
+        { name: 'Version', value: 'version' },
         { name: 'Mis à jour', value: 'updatedAt' }
     ];
 
     public orderBy: 'asc' | 'desc' = 'desc';
 
+    public filtered(list: any[]) {
+        return list
+            .filter(a => {
+                const searchTerm = this.search.toLowerCase();
+                return (
+                    a.name.toLowerCase().includes(searchTerm)
+                );
+            }
+            )
+            .sort((a, b) => {
+                const aValue = a[this.filterOn];
+                const bValue = b[this.filterOn];
+                if (this.orderBy === 'asc') {
+                    return aValue > bValue ? 1 : -1;
+                }
+
+                return aValue < bValue ? 1 : -1;
+            });
+    }
+
     public toggleOrder() {
         this.orderBy = this.orderBy === 'asc' ? 'desc' : 'asc';
+    }
+
+    public onDragStart(agent: any) {
+        this.elementDragged = agent;
+    }
+
+    public onDragEnd() {
+        this.elementDragged = null;
+    }
+
+    public onDragOver(ev: DragEvent) {
+        (ev.currentTarget as HTMLElement)?.classList.add('dropping');
+        ev.preventDefault();
+    }
+
+    public onDragLeave(ev: DragEvent) {
+        (ev.currentTarget as HTMLElement)?.classList.remove('dropping');
+    }
+
+    public onDropToDataset() {
+        const agent = this.elementDragged;
+        if (agent) {
+            this.modelsGenerated.push(agent);
+        }
+    }
+
+    public onDropToTrain() {
+        const agent = this.elementDragged;
+        if (agent) {
+            this.modelsGenerated = this.modelsGenerated.filter(a => a._id !== agent._id);
+            this.modelsToTrain.push(agent);
+        }
     }
 }
